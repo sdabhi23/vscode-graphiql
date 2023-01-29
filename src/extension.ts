e77339c9a5959d83dd7ff2474b6c0fb4b5ed78dd
shrey <shrey.dabhi23@gmail.com>
2023-01-29 10:37:03 +0530

    js csp update

--- src/extension.ts
+++ src/extension.ts
@@ -171,7 +171,7 @@ class GraphiqlPanel {
 					Use a content security policy to only allow loading images from https or from our extension directory,
 					and only allow scripts that have a specific nonce.
 				-->
-				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; connect-src *; font-src data:; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
+				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; connect-src *; font-src data:; style-src ${webview.cspSource} 'unsafe-inline'; script-src 'nonce-${nonce}' 'unsafe-eval';">
 				<meta name="viewport" content="width=device-width, initial-scale=1.0">
 				<link href="${stylesResetUri}" rel="stylesheet">
 				<link href="${stylesGraphiqlUri}" rel="stylesheet">
